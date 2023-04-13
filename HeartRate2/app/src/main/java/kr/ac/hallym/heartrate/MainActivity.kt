0f73f73cc9bd3934a26c4eaad92053a8dbcb9a11
BADA LEE <80818640+lbd0@users.noreply.github.com>
2023-04-13 16:18:43 +0900

    파이어베이스에 데이터 저장 형식 변경

--- HeartRate2/app/src/main/java/kr/ac/hallym/heartrate/MainActivity.kt
+++ HeartRate2/app/src/main/java/kr/ac/hallym/heartrate/MainActivity.kt
@@ -85,7 +85,7 @@ class MainActivity : AppCompatActivity() {
                 binding.heartRate.text = String.format("%.1f", it)
 
                 val myRef = firebaseDatabase.getReference("bpm")
-                myRef.push().setValue(it)
+                myRef.push().setValue(it.toString() + "bpm")
             }
         }
     }
